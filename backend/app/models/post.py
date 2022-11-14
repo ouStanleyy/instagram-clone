6613fd282ee68f7f9208029a6aef935ba2b1f9a2
stanley <ou.stanley@yahoo.com>
2022-11-14 18:54:18 -0500

    Added comments table to models.

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -4,7 +4,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Post(db.Model):
     '''
     Relationships:
-        Post has many Comments, Likes, Replies, Posts, Stories
+        Post has many Comments, Likes, Stories
     '''
     __tablename__ = 'posts'
 
@@ -12,10 +12,11 @@ class Post(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"))
     caption = db.Column(db.Text(2200))
 
-    user = db.relationship("User", back_populates='posts')
+    user = db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comment", back_populates="post", cascade="all, delete-orphan", passive_deletes=True)
 
     def to_dict(self):
         return {
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -31,6 +31,7 @@ class User(db.Model, UserMixin):
     is_private = db.Column(db.Boolean, nullable=False, default=False)
 
     posts = db.relationship("Post", back_populates="user", cascade="all, delete-orphan", passive_deletes=True)
+    comments = db.relationship("Comment", back_populates="user", cascade="all, delete-orphan", passive_deletes=True)
 
     @property
     def password(self):
