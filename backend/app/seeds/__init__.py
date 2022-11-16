63d75fcf22f1c1385dea19508531af8dfd52f31d
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-11-16 11:58:14 -0500

    Added likes seeders

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .posts import seed_posts, undo_posts
 from .comments import seed_comments, undo_comments
+from .likes import seed_likes, undo_likes
 
 from app.models.db import db, environment, SCHEMA
 
@@ -22,6 +23,7 @@ def seed():
     seed_users()
     seed_posts()
     seed_comments()
+    seed_likes()
     # Add other seed functions here
 
 
@@ -31,4 +33,5 @@ def undo():
     undo_users()
     undo_posts()
     undo_comments()
+    undo_likes()
     # Add other undo functions here
