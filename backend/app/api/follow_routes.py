1f4c75afb6fea1cbff682b44dd04ef30531ca070
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2022-11-19 17:25:17 -0500

    delete follow done

--- backend/app/api/follow_routes.py
+++ backend/app/api/follow_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, redirect
 from flask_login import login_required, current_user
-from app.models import User, Follow, db
+from app.models import Follow, db
 
 
 follow_routes = Blueprint("follows", __name__)
@@ -19,3 +19,20 @@ def accept_follow(follow_id):
     follow.is_pending = False
     db.session.commit()
     return follow.to_dict()
+
+
+@follow_routes.route("/<int:follow_id>", methods=["DELETE"])
+@login_required
+def delete_follow(follow_id):
+    """
+    Undo pending request or unfollow
+
+    Reject request or remove follower
+    """
+    follow = Follow.query.get_or_404(follow_id)
+    if current_user.id == follow.follower_id or current_user.id == follow.following_id:
+        db.session.delete(follow)
+        db.session.commit()
+        return "Successfully deleted"
+    return redirect("../auth/unauthorized")
+
