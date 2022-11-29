d0e09be96d145925e3e34eb46ed4a99d8c8201c3
stanley <ou.stanley@yahoo.com>
2022-11-28 19:09:43 -0500

    Updated routes in App.js. Updated CSS styling for User.

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -2,8 +2,13 @@ import Iphone from "./Iphone";
 import styles from "./Splash.module.css";
 import Form from "../Form/Form";
 
-const Splash = () => {
-  return (
+const Splash = ({ user }) => {
+  return user ? (
+    <div>
+      POST FEED TO GO HERE POST FEED TO GO HERE POST FEED TO GO HERE POST FEED
+      TO GO HERE POST FEED TO GO HERE POST FEED TO GO HERE POST FEED TO GO HERE
+    </div>
+  ) : (
     <div className={styles.splashContainer}>
       <Iphone />
       <Form />
