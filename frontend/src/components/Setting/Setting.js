8dc81199e9d779d4d5caa0caaa7b1868e0e5d5c8
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-11-29 20:20:21 -0500

    Setting update

--- frontend/src/components/Setting/Setting.js
+++ frontend/src/components/Setting/Setting.js
@@ -1,12 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
 import styles from "./Setting.module.css";
-import { Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 
 const Setting = () => {
   const user = useSelector((state) => state.session.user);
+
   return (
     <div className={styles.settingContainer}>
-      <div className={styles.settingNav}></div>
+      <div className={styles.settingNav}>
+        <NavLink to="/account/edit">Edit profile</NavLink>
+      </div>
       <div className={styles.settingOption}></div>
     </div>
   );
