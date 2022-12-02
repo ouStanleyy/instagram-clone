93eeeee5e3d9e7079e911439a5cbf0fa5ed890d6
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-12-01 19:48:30 -0500

    story carousel followings

--- frontend/src/components/Stories/StoryCarousel.js
+++ frontend/src/components/Stories/StoryCarousel.js
@@ -1,25 +1,11 @@
 import styles from "./StoryCarousel.module.css";
-import { getStoriesFeed } from "../../store/stories";
-import { getFollowing } from "../../store/follows";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const StoryCarousel = () => {
   /* For now, carousel for the followings, then replace for just stories */
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user);
-  // const stories = useSelector((state) => Object.values(state.stories));
   const followings = useSelector((state) =>
     Object.values(state.follows.following)
   );
-
-  console.log(followings);
-  useEffect(() => {
-    (async () => {
-      // await dispatch(getStoriesFeed(user.id));
-      await dispatch(getFollowing(user.id));
-    })();
-  }, [dispatch]);
 
   return <div className={styles.storyCarouselContainer}></div>;
 };
