6552f8fb97c68d10dbd1d136d2224aa6531f7aa4
stanley <ou.stanley@yahoo.com>
2022-12-04 21:34:28 -0500

    Added and styled search tab header.

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import styles from "./Search.module.css";
 
 const Search = ({ hideSearch }) => {
+  const [searchVal, setSearchVal] = useState("");
+
   return (
     <div
       className={`${styles.searchContainer} ${hideSearch && styles.hideSearch}`}
-    ></div>
+    >
+      <div className={styles.searchHeader}>
+        <h2>Search</h2>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchVal}
+          onChange={(e) => setSearchVal(e.target.value)}
+          className={styles.searchInput}
+        />
+      </div>
+      <div className={styles.searchResults}></div>
+    </div>
   );
 };
 
