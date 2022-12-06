0177e224abc10a50a876d3ee959b888dd1c7a145
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-12-05 21:43:15 -0500

    added seeeds

--- backend/app/seeds/media.py
+++ backend/app/seeds/media.py
@@ -226,6 +226,186 @@ medias = [
         "post_id": 44,
         "url": "https://tofuu.getjusto.com/orioneat-local/resized2/oyRHtR6bZDqqPRrZD-1200-1200.jpeg",
     },
+    {
+        "post_id": 45,
+        "url": "https://c4.wallpaperflare.com/wallpaper/175/589/120/kobe-bryant-computer-wallpaper-preview.jpg",
+    },
+    {
+        "post_id": 46,
+        "url": "https://static01.nyt.com/images/2020/01/28/sports/28KOBE-LEBRON-1/28KOBE-LEBRON-1-mobileMasterAt3x.jpg",
+    },
+    {
+        "post_id": 47,
+        "url": "https://i.pinimg.com/736x/59/9b/81/599b814e5c9ddc8c137b826aba03e714.jpg",
+    },
+    {
+        "post_id": 48,
+        "url": "https://archziner.com/wp-content/uploads/2020/09/kobe-sitting-on-a-bench-under-showers-cool-kobe-bryant-wallpapers-championship-trophy-on-the-floor-in-front-of-him-wearing-purple-jacket.jpg",
+    },
+    {
+        "post_id": 49,
+        "url": "https://archziner.com/wp-content/uploads/2020/09/black-and-white-photo-of-kobe-sitting-holding-a-basketball-photographed-from-the-side-kobe-wallpaper-black-background.jpg",
+    },
+    {
+        "post_id": 50,
+        "url": "https://archziner.com/wp-content/uploads/2020/09/kobe-bryant-dunking-on-lebron-james-dwyane-wade-in-the-back-kobe-bryant-and-gigi-wallpaper-wearing-yellow-lakers-jersey.jpg",
+    },
+    {
+        "post_id": 51,
+        "url": "http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/4c0bb4b5cec87775501f079252b93481",
+    },
+    {
+        "post_id": 51,
+        "url": "https://img.bleacherreport.net/img/images/photos/001/567/922/138693133_crop_north.jpg?1328972994&w=3072&h=2048",
+    },
+    {
+        "post_id": 51,
+        "url": "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/boh0rc9vrqldik1sq9qf/kobe-bryant-jeremy-lin?fimg-ssr-default",
+    },
+    {
+        "post_id": 52,
+        "url": "https://media.architecturaldigest.com/photos/61b0ce48dccdb75fa170f8f7/16:9/w_2560%2Cc_limit/PurpleCherry_Williams_0012.jpg",
+    },
+    {
+        "post_id": 53,
+        "url": "https://media.architecturaldigest.com/photos/620bd9289f3070a4c7c4992a/master/w_2580%2Cc_limit/Tivoli_0191%2520copy.jpg",
+    },
+    {
+        "post_id": 53,
+        "url": "https://media.architecturaldigest.com/photos/620bd92b9494f79a45ba0ded/master/w_2580%2Cc_limit/Tivoli_0277%2520copy.jpg",
+    },
+    {
+        "post_id": 53,
+        "url": "https://media.architecturaldigest.com/photos/620bd9311de6774a1e2776d7/master/w_2580%2Cc_limit/Tivoli_0578%2520copy.jpg",
+    },
+    {
+        "post_id": 53,
+        "url": "https://media.architecturaldigest.com/photos/620bd9319494f79a45ba0df0/master/w_2580%2Cc_limit/Tivoli_0628%2520copy.jpg",
+    },
+    {
+        "post_id": 53,
+        "url": "https://media.architecturaldigest.com/photos/620bd932026e6f5992462db5/master/w_2580%2Cc_limit/Tivoli_0660%2520copy.jpg",
+    },
+    {
+        "post_id": 54,
+        "url": "https://media.architecturaldigest.com/photos/63084796bab7603078f00328/master/w_1920%2Cc_limit/AlexanderDesign_Ohana_461.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf9ac6c719e8ddb639db/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_84.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf9f8301919648ec046c/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_189.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf9fde9d9bfbc5c424ec/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_11%25201.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf94f5e42ed57ef9275a/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_46%25201.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf99de9d9bfbc5c424ea/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_110.jpg",
+    },
+    {
+        "post_id": 55,
+        "url": "https://media.architecturaldigest.com/photos/6388cf9463d52d63aab3e1eb/master/w_2580%2Cc_limit/22060_press_jcarlyle_9-16-22_09%25201.jpg",
+    },
+    {
+        "post_id": 56,
+        "url": "https://media.architecturaldigest.com/photos/637c2b32a38f1233b1bfd8bd/master/w_2580%2Cc_limit/Madrigal_House_18.jpg",
+    },
+    {
+        "post_id": 56,
+        "url": "https://media.architecturaldigest.com/photos/637c2b3405aee881010b77e4/master/w_2580%2Cc_limit/Madrigal_House_17.jpg",
+    },
+    {
+        "post_id": 56,
+        "url": "https://media.architecturaldigest.com/photos/637c2b2255354fd6222df827/master/w_2580%2Cc_limit/Madrigal_House_3.jpg",
+    },
+    {
+        "post_id": 56,
+        "url": "https://media.architecturaldigest.com/photos/637c2b190d1db0a30f58003c/master/w_2580%2Cc_limit/Madrigal_House_15.jpg",
+    },
+    {
+        "post_id": 57,
+        "url": "https://media.architecturaldigest.com/photos/6388fe6d7c7d908b8ba494c1/1:1/w_960,c_limit/Guangxi%20China%20Resources%20Tower_(c)%20Arch-Exist_07.jpg",
+    },
+    {
+        "post_id": 57,
+        "url": "https://media.architecturaldigest.com/photos/6388fe6e7abfaa8863f3be36/master/w_1600,c_limit/Guangxi%20China%20Resources%20Tower_(c)%20Arch-Exist_08.jpg",
+    },
+    {
+        "post_id": 57,
+        "url": "https://media.architecturaldigest.com/photos/6388fe6cde9d9bfbc5c424f7/master/w_1280,c_limit/Guangxi%20China%20Resources%20Tower_(c)%20Arch-Exist_06.jpg",
+    },
+    {
+        "post_id": 58,
+        "url": "https://media.architecturaldigest.com/photos/6376556a49247ea910900082/16:9/w_2240,c_limit/toftan_cred_Mikael_Svensson.jpg",
+    },
+    {
+        "post_id": 58,
+        "url": "https://media.architecturaldigest.com/photos/63765644635a1f2c06ac68e5/master/w_1600,c_limit/Kvarntorp_1910LS_0037.jpg",
+    },
+    {
+        "post_id": 58,
+        "url": "https://media.architecturaldigest.com/photos/637658a62ae6947686c103ca/master/w_1600,c_limit/Innerg%C3%A5rd%20i%20Askersund.jpg",
+    },
+    {
+        "post_id": 59,
+        "url": "https://media.architecturaldigest.com/photos/638779f066ac9bb6610acf22/1:1/w_960,c_limit/AD0123_TUPKER_1%20copy.jpg",
+    },
+    {
+        "post_id": 59,
+        "url": "https://media.architecturaldigest.com/photos/638779f421b38b119a99ce25/master/w_1600,c_limit/AD0123_TUPKER_16%20copy.jpg",
+    },
+    {
+        "post_id": 59,
+        "url": "https://media.architecturaldigest.com/photos/638779f4f94546381283d0a0/master/w_1600,c_limit/AD0123_TUPKER_15%20copy.jpg",
+    },
+    {
+        "post_id": 59,
+        "url": "https://media.architecturaldigest.com/photos/63878559e7f5a4a139386e34/master/w_1600,c_limit/AD0123_TUPKER_4%20copy.jpg",
+    },
+    {
+        "post_id": 59,
+        "url": "https://media.architecturaldigest.com/photos/638779f67a6dc63ff504aed5/master/w_1600,c_limit/AD0123_TUPKER_21%20copy.jpg",
+    },
+    {
+        "post_id": 60,
+        "url": "https://media.architecturaldigest.com/photos/638502144e358184e5cfff8b/16:9/w_2240,c_limit/3S2A0939_HDR-Pano-Edit%20(1).jpg",
+    },
+    {
+        "post_id": 61,
+        "url": "https://dm0qx8t0i9gc9.cloudfront.net/watermarks/video/GTYSdDW/videoblocks-computer-hacking-in-process-cyber-security-concept_h-l3zbu4xb__0a7b0b12279471d24693c0d1a1aa7c82__P360.mp4",
+    },
+    {
+        "post_id": 62,
+        "url": "https://codingbootcamps.io/wp-content/uploads/m2.png",
+    },
+    {
+        "post_id": 63,
+        "url": "https://v16m-webapp.tiktokcdn-us.com/a056508324332112b8c176bf25977d78/638efe56/video/tos/useast5/tos-useast5-ve-0068c001-tx/f9ab0e7c1f2f476d81c8d1041127be55/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=588&bt=294&cs=0&ds=3&ft=ebtHKH-qMyq8ZNAeThe2NKidfl7Gb&mime_type=video_mp4&qs=0&rc=NTM6OmczaDY6aTppNWdpaEBpajxoNzk6Zmg8ZzMzZzczNEAyMS8wMC1eXmIxXjQ1YjVjYSNgcDNycjRnazNgLS1kMS9zcw%3D%3D&l=20221206023231663ABAB0AB2824001F09",
+    },
+    {
+        "post_id": 64,
+        "url": "https://v16m-webapp.tiktokcdn-us.com/e65e650a23552294f2714a31d8ba475f/638efee4/video/tos/useast5/tos-useast5-pve-0068-tx/c8b28610bd184408b457d230d3bb685b/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=596&bt=298&cs=0&ds=3&ft=ebtHKH-qMyq8ZRjeThe2Nm6Dfl7Gb&mime_type=video_mp4&qs=0&rc=ZWhkOTplZzxpNWczODM4NEBpam43amU6ZjlmZDMzZzczNEBeM19hNTNhXmAxLmM2L18wYSMuXmRwcjQwYWxgLS1kMS9zcw%3D%3D&l=202212060234493397792145207C0017AB",
+    },
+    {
+        "post_id": 65,
+        "url": "https://v16m-webapp.tiktokcdn-us.com/8155445494c63d2541b8a90565814192/638efeed/video/tos/alisg/tos-alisg-pve-0037c001/63033563a0174acf91cd58410ef7605a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1904&bt=952&cs=0&ds=3&ft=ebtHKH-qMyq8ZaEeThe2NiD-fl7Gb&mime_type=video_mp4&qs=0&rc=M2U2ZDs7N2U3Mzk2aGQ7OkBpamhwOjo6ZjZoZjMzODczNEA2YTY0Li8zXl8xLy02Ni4yYSMzYi9zcjRnLmlgLS1kMS1zcw%3D%3D&l=20221206023546327608FDA72D02001D91",
+    },
+    {
+        "post_id": 66,
+        "url": "https://v16m-webapp.tiktokcdn-us.com/8788224810a5ef43aada0d826b9e1e6e/638f0057/video/tos/useast2a/tos-useast2a-ve-0068c004/921dc18515504979a9e8623720f1ea6f/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4532&bt=2266&cs=0&ds=3&ft=ebtHKH-qMyq8ZvneThe2Nbm~fl7Gb&mime_type=video_mp4&qs=0&rc=NGc4Z2U0PGdnaWU4NGhpZUBpM3g0d2U6ZjplOTMzNzczM0BiYWAvY2JfNTUxNTMxMzJjYSNqazBjcjRnLWJgLS1kMTZzcw%3D%3D&l=20221206024145EFCB99FF73BBAE0020F8",
+    },
+    {
+        "post_id": 67,
+        "url": "https://www.multidots.com/wp-content/uploads/2020/01/code-quality-standard.png?quality=90",
+    },
 
 
 
