3f5bbb10dd9ea5ad3c3212da85b4d7718aa47aa8
stanley <ou.stanley@yahoo.com>
2022-12-07 22:28:21 -0500

    DM works between 2 users. Styled some of the component.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from flask_socketio import SocketIO, emit
-from .models import db, User
+from .models import db, User, Message
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.post_routes import post_routes
@@ -126,6 +126,8 @@ def connected():
 def handle_message(data):
     """event listener when client types a message"""
     print("data from the front end: ",data['message'], data['room'])
+    db.session.add(Message(user_id=data['uid'], room_id=data['room'], message=data['message']))
+    db.session.commit()
     emit('message',{'message':data['message'],'sid':request.sid},room=data['room'])
     # emit(event,{'data':data,'id':request.sid},broadcast=True)
 
--- frontend/src/components/Messages/Messages.js
+++ frontend/src/components/Messages/Messages.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, Switch, Route } from "react-router-dom";
 import { getRooms } from "../../store/rooms";
-import DirectMessage from "./DirectMessage";
+import Conversation from "./Conversation";
 import { ProfilePicture } from "../Elements";
 import styles from "./Messages.module.css";
 
@@ -82,7 +82,7 @@ const Messages = () => {
       <div className={styles.convoContainer}>
         <Switch>
           <Route path="/messages/:roomId" exact={true}>
-            <DirectMessage />
+            <Conversation sessionUser={sessionUser} rooms={rooms} />
           </Route>
         </Switch>
       </div>
