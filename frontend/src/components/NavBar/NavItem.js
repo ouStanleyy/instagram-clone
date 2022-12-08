5a42cb0d91b100f1ee581bd1920e036ea59f6baa
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-12-08 11:01:38 -0500

    search bar focus when opened

--- frontend/src/components/NavBar/NavItem.js
+++ frontend/src/components/NavBar/NavItem.js
@@ -3,11 +3,18 @@ import { useSelector } from "react-redux";
 import styles from "./NavItem.module.css";
 import ProfilePicture from "../Elements/ProfilePIcture";
 
-const NavItem = ({ type, showSearch, hideSearch, showNotification, hideNotification, hasNotification }) => {
+const NavItem = ({
+  type,
+  showSearch,
+  hideSearch,
+  showNotification,
+  hideNotification,
+  hasNotification,
+}) => {
   const user = useSelector((state) => state.session.user);
   const isLogo = type === "Logo" || type === "Instagram";
   const style = isLogo ? styles.logo : styles.navItem;
-  console.log(hasNotification)
+  console.log(hasNotification);
   const profilePicture = (
     <>
       <div className={styles.profilePicture}>
@@ -23,7 +30,9 @@ const NavItem = ({ type, showSearch, hideSearch, showNotification, hideNotificat
       {isLogo && <div className={styles.instagramLogo}>{icons["Logo"]}</div>}
       <div className={`${styles.svgContainer} ${isLogo && styles.hideSvg}`}>
         {icons[type]}
-        {(type === "Notifications" && hasNotification) &&<div className={styles.redCircle}></div>}
+        {type === "Notifications" && hasNotification && (
+          <div className={styles.redCircle}></div>
+        )}
       </div>
       <span>{isLogo ? "" : type}</span>
     </>
@@ -32,9 +41,17 @@ const NavItem = ({ type, showSearch, hideSearch, showNotification, hideNotificat
   return (
     <div
       className={`${style} ${showSearch && !hideSearch && styles.hideNavItem}
-                  ${type === "Search" && !showNotification && styles.searchBorder}
+                  ${
+                    type === "Search" &&
+                    !showNotification &&
+                    styles.searchBorder
+                  }
                   ${showNotification && !hideNotification && styles.hideNavItem}
-                  ${type === "Notifications" && !showSearch && styles.notifBorder}
+                  ${
+                    type === "Notifications" &&
+                    !showSearch &&
+                    styles.notifBorder
+                  }
 
 
       `}
