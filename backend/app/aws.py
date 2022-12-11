e710551e6461abd37dc24244af687258bc31d535
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-12-11 16:00:13 -0500

    please work

--- backend/app/aws.py
+++ backend/app/aws.py
@@ -81,15 +81,15 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
     """
 
     # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+    # if object_name is None:
+    #     object_name = os.path.basename(file_name)
 
     # Upload the file
     s3_client = boto3.client('s3',
                     aws_access_key_id=os.environ.get("S3_KEY"),
                     aws_secret_access_key=os.environ.get("S3_SECRET"))
     try:
-        response = s3_client.upload_fileobj(file_name, bucket, object_name)
+        response = s3_client.upload_file("/opt/render/project/src/frontend/public/assets", bucket, object_name)
     except ClientError as e:
         logging.error(e)
         return False
